19c3d25a6e30c8eda4009cb97a799dde8cc95a6b
nextagent123 <caovantaihcm@gmail.com>
2026-07-22 22:41:53 +0700

    update

--- PlaneIcon.js
+++
@@ -1,7 +0,0 @@
-export default function PlaneIcon({ size = 20, color = "currentColor", opacity = 1 }) {
-  return (
-    <svg width={size} height={size} viewBox="0 0 24 24" fill={color} opacity={opacity}>
-      <path d="M21 16v-2l-8-5V3.5a1.5 1.5 0 0 0-3 0V9l-8 5v2l8-2.5V19l-2.5 1.5V22l4-1 4 1v-1.5L13 19v-5.5l8 2.5Z" />
-    </svg>
-  );
-}
--- icons.js
+++
@@ -1,7 +0,0 @@
-// Icon path data for the 4 recurring metrics, shared across Home/Company/Khối.
-export const METRIC_ICON_PATHS = {
-  gmv: ["M12 2v20", "M17 5.5H9.5a3 3 0 0 0 0 6h5a3 3 0 0 1 0 6H6"],
-  dt: ["M3 3v18h18", "M7 14l4-4 3 3 5-6"],
-  ln: ["M12 2 4 5v6c0 5 3.5 8 8 10 4.5-2 8-5 8-10V5l-8-3Z"],
-  dv: ["M4 7h13l-3-3", "M20 17H7l3 3"],
-};
